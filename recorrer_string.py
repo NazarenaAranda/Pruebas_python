66175c4238bca648e56e9d49392d74765683f079
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-10 01:31:17 -0300

    imprimir letra a letra en mayus

---
+++ recorrer_string.py
@@ -0,0 +1,8 @@
+def main():
+
+    frase = input("Escribe una frase: ")
+    for caracter in frase:
+        print(caracter.upper()) #va a imprimir letra por letra en mayuscula
+
+if __name__ == "__main__":
+    main()
