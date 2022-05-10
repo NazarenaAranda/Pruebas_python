7f9fae2374a8e49c05bcb356e6989e4dc7b88d7f
Nazarena Aranda <98347450+NazarenaAranda@users.noreply.github.com>
2022-05-10 15:29:29 -0300

    lambda

---
+++ lamda.py
@@ -0,0 +1,6 @@
+def palindromo(string):
+    return string == string[::-1] #codigo con una funcion normal
+print(palindromo("ana"))
+
+#palindromo = lambda string: string == string[::-1] #codigo con lambda
+#print(palindromo("ana"))
