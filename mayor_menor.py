e532d7f54c3c4fd8b0271f97af374f6d895c60f3
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-09 21:28:29 -0300

    mayor o menor de edad

--- mayor_menor.py
+++ mayor_menor.py
@@ -1,5 +1,7 @@
 edad = int(input("Escribe tu edad:" ))
 if edad > 17:
     print("Eres mayor de edad")
+elif edad == 17:
+    print("Te falta poquito para ser mayor de edad")
 else:
     print("Eres menor de edad")
