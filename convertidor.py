21805aeea496071c7c549607136cd9528850e040
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-09 21:31:44 -0300

    comentarios

--- convertidor.py
+++ convertidor.py
@@ -1,9 +1,9 @@
-pesos = input("Cuantos pesos uruguayos tienes?" )
-pesos = float(pesos)
+pesos = input("Cuantos pesos uruguayos tienes?" ) #input es para que el usuario ingrese algo
+pesos = float(pesos) #con float pasas a decimal
 valor_dolar = 41.54
 dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+dolares = round(dolares, 2) #round sirve para redondear el numero los lugares que le aclaramos
+dolares = str(dolares) #str pasa el numero a texto
 print("Tienes $" + dolares + " dolares")
 
 dolares = input("Cuantos dolares tienes?" )
