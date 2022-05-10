b3526272d5c1c7bf0bd1c7447d1fc98ab2005456
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-10 01:39:25 -0300

    usando break

---
+++ imprimir_sin_llegar_al_final.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(1000000):
+        print(i)
+        if i == 4567: #todos los numeros paren tienen de resto 0
+            break
+
+
+if __name__ == "__main__":
+    main()
