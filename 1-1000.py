ba160a6d98c488e317f1358f0e3602164740634d
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-10 01:16:40 -0300

    imprimir del 1 al 1000

---
+++ 1-1000.py
@@ -0,0 +1,6 @@
+def main():
+    for contador in range(1001): #va a imprimir del 0 hasta el 1000 ha
+        print(contador)
+
+if __name__ == "__main__":
+    main()
--- elevar_a_2.py
+++ elevar_a_2.py
@@ -6,7 +6,7 @@ def main():
     
     while potencia_2 < LIMITE:
         print("2 elevado a " + str(contador) + " es igual a: " + str(potencia_2))
-        contador = contador + 1
+        contador += 1
         potencia_2 = 2**contador
 
 
