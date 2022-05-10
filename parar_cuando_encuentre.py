4fcfc01abf26a0b17acbe30b1ae26d4fbac5fa00
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-10 01:48:37 -0300

    cuando encuentre la "o", deja de imprimir

---
+++ parar_cuando_encuentre.py
@@ -0,0 +1,11 @@
+def main():
+    frase = input("Escribe una frase: ")
+    for letra in frase:
+        print(letra)
+        if letra == 'o': 
+            break
+        
+
+
+if __name__ == "__main__":
+    main()
