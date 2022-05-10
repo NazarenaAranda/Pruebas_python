1c08bd9fd58b859d62b2c1388e8e8a70fe2f0ab6
NazarenaAranda <Nazarenaaranda15@gmail.com>
2022-05-09 21:00:11 -0300

    cambio de pesos a dolares y dolares a pesos

--- convertidor.py
+++ convertidor.py
@@ -1,7 +1,15 @@
-pesos = input("Cuantos pesos Colombianos tienes?" )
+pesos = input("Cuantos pesos uruguayos tienes?" )
 pesos = float(pesos)
-valor_dolar = 4087
+valor_dolar = 41.54
 dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares + " dolares")
+
+dolares = input("Cuantos dolares tienes?" )
+dolares = float(dolares)
+valor_pesos = 0.024
+pesos = dolares / valor_pesos
+pesos = round(pesos, 2)
+pesos = str(pesos)
+print("Tienes $" + pesos + " pesos uruguayos")
