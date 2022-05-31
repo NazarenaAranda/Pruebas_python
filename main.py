8210e9411de865dbcafe100b17b57412a3c1f304
Nazarena Aranda <98347450+NazarenaAranda@users.noreply.github.com>
2022-05-31 12:03:26 -0300

    cosas

---
+++ main.py
@@ -0,0 +1,8 @@
+def suma(x, y):
+    """ Funcion que toma 2 argumentos y devuelve la suma de los mismos.
+    Args: 
+    x- Primer valor a sumar (puede ser int o float) 
+    y- Segundo valor a sumar (puede ser int o float)
+    return: x+y"""
+
+    return x + y
