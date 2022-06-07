36d7e42a3b74fedfb179484cef6332df907282aa
Nazarena Aranda <98347450+NazarenaAranda@users.noreply.github.com>
2022-06-07 18:55:46 -0300

    lamda

--- lamda.py
+++ lamda.py
@@ -1,6 +1,11 @@
-def palindromo(string):
-    return string == string[::-1] #codigo con una funcion normal
-print(palindromo("ana"))
-
-#palindromo = lambda string: string == string[::-1] #codigo con lambda
+#def palindromo(string):
+    #return string == string[::-1] #codigo con una funcion normal
 #print(palindromo("ana"))
+
+palindromo = lambda string: string == string[::-1] #codigo con lambda
+print(palindromo("ana"))
+#"palindromo" - identificador
+# "lambda string" - argumento
+#"string == string[::-1]" - expresión
+
+#lambda argumentos: expresión - Se usa lamdba en vez de def y en python esta funcion es una sola linea de codigo
